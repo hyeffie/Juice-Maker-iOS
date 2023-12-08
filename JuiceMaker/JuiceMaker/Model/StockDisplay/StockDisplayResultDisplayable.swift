5dfe36d7162f6132afbbcc823abc2f851bd8612d
Effie <effie.thedev@gmail.com>
2023-12-08 20:43:43 +0900

    feat #4: StockDisplayResultDisplayable 선언

---
+++ JuiceMaker/JuiceMaker/Model/StockDisplay/StockDisplayResultDisplayable.swift
@@ -0,0 +1,10 @@
+//
+//  StockDisplayResultDisplayable.swift
+//  JuiceMaker
+//
+//  Created by Effie on 12/8/23.
+//
+
+protocol StockDisplayResultDisplayable {
+    func stockDisplayResultDisplay(viewModel: StockDisplay.ViewModel)
+}
