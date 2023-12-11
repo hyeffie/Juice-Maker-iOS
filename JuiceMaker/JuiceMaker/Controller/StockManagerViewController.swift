ac7837f78f1b0b062ece306e27bc5d4fffe43c34
Effie <effie.thedev@gmail.com>
2023-12-11 11:20:05 +0900

    feat #7: StockManagerViewController 구현 추가
    - use case 프로퍼티
    - custom init

--- JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
@@ -8,5 +8,19 @@
 import UIKit
 
 final class StockManagerViewController: UIViewController, StoryboardIdentifiale {
+    private var stockDisplay: StockDisplay?
     
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    init?(coder: NSCoder, fruitStore: FruitStore) {
+        self.stockDisplay = StockDisplay(fruitStore: fruitStore)
+        super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
 }
