e7984017342c6e620468bc268258f1f54f46e2a1
Effie <effie.thedev@gmail.com>
2023-12-15 18:07:53 +0900

    feat: StockModifier 구현 완성

--- JuiceMaker/JuiceMaker/UseCase/StockModifier.swift
+++ JuiceMaker/JuiceMaker/UseCase/StockModifier.swift
@@ -8,15 +8,20 @@
 final class StockModifier {
     private let fruitStore: FruitStore
     
+    var resultConverter: StockModifierResultConvertable?
+    
     init(fruitStore: FruitStore) {
         self.fruitStore = fruitStore
     }
     
     func updateStock(of fruit: Fruit, to newCount: Int) {
         do {
-            let _ = try fruitStore.updateStock(of: fruit, to: newCount)
+            let updatedStock = try fruitStore.updateStock(of: fruit, to: newCount)
+            let result = StockModifierModel.Result(stock: updatedStock)
+            resultConverter?.convertResult(result)
         } catch {
-            
+            let result = StockModifierModel.Result(stock: nil)
+            resultConverter?.convertResult(result)
         }
     }
 }
---
+++ JuiceMaker/JuiceMaker/UseCase/StockModifierResultConvertable.swift
@@ -0,0 +1,10 @@
+//
+//  StockModifierResultConvertable.swift
+//  JuiceMaker
+//
+//  Created by Effie on 12/15/23.
+//
+
+protocol StockModifierResultConvertable {
+    func convertResult(_ result: StockModifierModel.Result)
+}
