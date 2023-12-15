6dc078cfc1eb0023a29509feead398e5577ba577
Effie <effie.thedev@gmail.com>
2023-12-15 16:16:59 +0900

    chore: vc action 메서드 접근 지정자 설정

--- JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
@@ -44,35 +44,35 @@ final class JuiceMakerViewController: UIViewController, StoryboardIdentifiale {
         stockDisplay?.displayStock()
     }
     
-    @IBAction func makeStrawberryBananaJuice(_ sender: UIButton) {
+    @IBAction private func makeStrawberryBananaJuice(_ sender: UIButton) {
         juiceMaker?.makeJuice(flavor: .strawberryBanana)
     }
     
-    @IBAction func makeMangoKiwiJuice(_ sender: UIButton) {
+    @IBAction private func makeMangoKiwiJuice(_ sender: UIButton) {
         juiceMaker?.makeJuice(flavor: .mangoKiwi)
     }
     
-    @IBAction func makeStrawberryJuice(_ sender: UIButton) {
+    @IBAction private func makeStrawberryJuice(_ sender: UIButton) {
         juiceMaker?.makeJuice(flavor: .strawberry)
     }
     
-    @IBAction func makeBananaJuice(_ sender: UIButton) {
+    @IBAction private func makeBananaJuice(_ sender: UIButton) {
         juiceMaker?.makeJuice(flavor: .banana)
     }
     
-    @IBAction func makePineappleJuice(_ sender: UIButton) {
+    @IBAction private func makePineappleJuice(_ sender: UIButton) {
         juiceMaker?.makeJuice(flavor: .pineapple)
     }
     
-    @IBAction func makeKiwiJuice(_ sender: UIButton) {
+    @IBAction private func makeKiwiJuice(_ sender: UIButton) {
         juiceMaker?.makeJuice(flavor: .kiwi)
     }
     
-    @IBAction func makeMangoJuice(_ sender: UIButton) {
+    @IBAction private func makeMangoJuice(_ sender: UIButton) {
         juiceMaker?.makeJuice(flavor: .mango)
     }
     
-    @IBAction func editStock(_ sender: UIBarButtonItem) {
+    @IBAction private func editStock(_ sender: UIBarButtonItem) {
         router?.routeToNextViewController()
     }
     
