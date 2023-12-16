f45b25c8aacc576b9135c9d48743470903baa4ef
Effie <effie.thedev@gmail.com>
2023-12-16 13:24:40 +0900

    feat: outlet, action 연결 및 구현

--- JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
@@ -18,6 +18,16 @@ final class StockManagerViewController: UIViewController, StoryboardIdentifiable
     
     @IBOutlet private weak var mangoStockLabel: UILabel!
     
+    @IBOutlet private weak var strawberryCountStepper: UIStepper!
+    
+    @IBOutlet private weak var bananaCountStepper: UIStepper!
+    
+    @IBOutlet private weak var pineappleCountStepper: UIStepper!
+    
+    @IBOutlet private weak var kiwiCountStepper: UIStepper!
+    
+    @IBOutlet private weak var mangoCountStepper: UIStepper!
+    
     private let stockDisplay: StockDisplay?
     
     private let stockModifier: StockModifier?
@@ -37,11 +47,32 @@ final class StockManagerViewController: UIViewController, StoryboardIdentifiable
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
         stockDisplay?.displayStock()
     }
     
     @IBAction private func completeManaging(_ sender: Any) {
         self.dismiss(animated: true)
+    }
+    
+    @IBAction func changeStockCount(_ sender: UIStepper) {
+        let fruitType: Fruit
+        switch sender {
+        case strawberryCountStepper: 
+            fruitType = .strawberry
+        case bananaCountStepper: 
+            fruitType = .banana
+        case pineappleCountStepper: 
+            fruitType = .pineapple
+        case kiwiCountStepper: 
+            fruitType = .kiwi
+        case mangoCountStepper: 
+            fruitType = .mango
+        default: 
+            return
+        }
+        let newCount = Int(sender.value)
+        self.stockModifier?.updateStock(of: fruitType, to: newCount)
     }
     
     private func setUp() {
@@ -52,6 +83,18 @@ final class StockManagerViewController: UIViewController, StoryboardIdentifiable
         let stockModifierResultConverter = StockModifierResultConverter()
         stockModifierResultConverter.display = self
         stockModifier?.resultConverter = stockModifierResultConverter
+    }
+    
+    private func setUI() {
+        [
+            strawberryCountStepper,
+            bananaCountStepper,
+            pineappleCountStepper,
+            kiwiCountStepper,
+            mangoCountStepper,
+        ].forEach { stepper in
+            stepper?.stepValue = 1.0
+        }
     }
 }
 
@@ -64,11 +107,32 @@ extension StockManagerViewController: StockDisplayResultDisplayable {
         self.pineappleStockLabel.text = "\(eachFruitCount.pineappleCount)"
         self.kiwiStockLabel.text = "\(eachFruitCount.kiwiCount)"
         self.mangoStockLabel.text = "\(eachFruitCount.mangoCount)"
+        
+        self.strawberryCountStepper.value = Double(eachFruitCount.strawberryCount)
+        self.bananaCountStepper.value = Double(eachFruitCount.bananaCount)
+        self.pineappleCountStepper.value = Double(eachFruitCount.pineappleCount)
+        self.kiwiCountStepper.value = Double(eachFruitCount.kiwiCount)
+        self.mangoCountStepper.value = Double(eachFruitCount.mangoCount)
     }
 }
 
 extension StockManagerViewController: StockModifierResultDisplayable {
     func displayModifiedStock(viewModel: StockModifierModel.ViewModel) {
-        
+        guard let newStock = viewModel.stock else {
+            
+            return
+        }
+        switch newStock.fruitType {
+        case .strawberry:
+            strawberryStockLabel.text = "\(newStock.count)"
+        case .banana:
+            bananaStockLabel.text = "\(newStock.count)"
+        case .pineapple:
+            pineappleStockLabel.text = "\(newStock.count)"
+        case .kiwi:
+            kiwiStockLabel.text = "\(newStock.count)"
+        case .mango:
+            mangoStockLabel.text = "\(newStock.count)"
+        }
     }
 }
